0e1ffc29b391a64ea0ce409f693b45e51511f66f
SudipGhara19 <sudipghara135@gmail.com>
2024-05-16 22:15:59 +0530

    created routes in the App.js for the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+import Projects from "./pages/Projects.jsx"
+import Signin from "./pages/Signin.jsx"
+import Signup from "./pages/Signup.jsx"
 
 function App() {
   return (
-    <h1 className="text-3xl text-center text-green-900">App</h1>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Home/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
