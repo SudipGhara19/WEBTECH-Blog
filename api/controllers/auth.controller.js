f129e0047643728d5a118c085a3306833c976ddc
SudipGhara19 <sudipghara135@gmail.com>
2024-05-20 02:01:53 +0530

    created signin api and send the user detail and jwt token with response except the password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,17 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcrypt';
+// import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
-
+//------------------------sign-up controller----------------------------
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        next(errorHandler(400, 'All fields are required.'))
+        return next(errorHandler(400, 'All fields are required.'))
     }
-
+    //hashing the password
     const hashedPassword = bcrypt.hashSync(password, 10);
 
     const newUser = new User({
@@ -19,9 +21,52 @@ export const signup = async (req, res, next) => {
     })
 
     try{
+        // save to mongoDB
         await newUser.save();
         return res.json("Signup successful.")
     }catch(err){
         next(err);
+    }
+}
+
+// ------------------------------sign in controller----------------------------------
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || email === ''){
+        next(errorHandler(400, 'All fields are required.'));
+    }
+
+    try{
+        // checking user has a account or not by email
+         const validUser = await User.findOne({email});
+         if(!validUser){
+            return next(errorHandler(400, 'User not found.'))
+         }
+         // checking the password
+         const validPassword = bcrypt.compareSync(password, validUser.password);
+         if(!validPassword){
+            return next(errorHandler(400, 'Invaild password.'))
+         }
+
+         //hinding password before sending
+         const {password: pass, ...rest} = validUser._doc;
+
+         // Generating jwt token
+         const token = jwt.sign(
+            {id: validUser._id},
+            process.env.JWT_SECRET,
+            {expiresIn: '1d'}
+         );
+
+         // sending token to the cookie and userdata with response
+         res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+         }).json(rest);
+
+
+    }catch(error){
+        console.log(error);
+        next(error);
     }
 }
