f079be7da675206103824af69ff991cd23527039
SudipGhara19 <sudipghara135@gmail.com>
2024-05-22 01:20:16 +0530

    added Dark mode - Light Mode feature by creationg a ThemeProvider.jsx component and provide the theme to the entire app. created ThemeSlice.js in redux to toggle over dark and light

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import {Provider} from 'react-redux';
 import { store, persistor } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
+import ThemeProvider from './components/ThemeProvider.jsx';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </PersistGate>
 );
