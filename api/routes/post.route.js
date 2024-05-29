68b18f9aa28fffcdfa925d3b645e39b9296065a7
SudipGhara19 <sudipghara135@gmail.com>
2024-05-30 00:19:40 +0530

    Created Post API for get the post on the bases of Query and also got the total posts and lastmonth posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getPosts } from '../controllers/post.controller.js';
 
 const postRoutes = express.Router();
 
 postRoutes.post('/create', verifyToken, create);
+postRoutes.get('/getposts', getPosts);
 
 export default postRoutes;
