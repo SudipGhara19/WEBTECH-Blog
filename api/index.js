b023cf4cddd4696d6db6ce937fed5bf497d4bbcf
SudipGhara19 <sudipghara135@gmail.com>
2024-07-17 19:53:32 +0530

    chages to make it accessable

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const port = 5001;
 const app = express();
 
 const corsOption = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }
